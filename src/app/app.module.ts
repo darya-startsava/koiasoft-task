abca000be89a0529aa720f9d7bfad37ffa5320eb
darya-startsava <daryastartsava@gmail.com>
2023-06-23 02:05:12 +0200

    feat: add Invitees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { MainSectionComponent } from './main-section/main-section.component';
@@ -40,6 +41,7 @@ import { TaskListComponent } from './task-list/task-list.component';
         MatFormFieldModule,
         FormsModule,
         ReactiveFormsModule,
+        MatTabsModule,
         SharedModule
     ],
     providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }],
--- src/app/right-side-section/right-side-section.component.ts
+++ src/app/right-side-section/right-side-section.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+export interface ParticipantsInterface {
+    name: string;
+    completed: boolean;
+    participant: Array<ParticipantInterface>;
+}
+
+export interface ParticipantInterface {
+    name: string;
+    completed: boolean;
+    title: string;
+}
 
 @Component({
-  selector: 'app-right-side-section',
-  templateUrl: './right-side-section.component.html',
-  styleUrls: ['./right-side-section.component.scss']
+    selector: 'app-right-side-section',
+    templateUrl: './right-side-section.component.html',
+    styleUrls: ['./right-side-section.component.scss']
 })
 export class RightSideSectionComponent {
+    search = new FormControl('');
+    allComplete: boolean = false;
 
+    participants: ParticipantsInterface = {
+        name: 'All',
+        completed: false,
+        participant: [
+            { name: 'Person 1', completed: false, title: 'Developer' },
+            { name: 'Person 2', completed: false, title: 'Developer' },
+            { name: 'Person 3', completed: false, title: 'Project Manager' },
+            { name: 'Person 4', completed: false, title: 'Tech lead' },
+            { name: 'Person 5', completed: false, title: 'QA' }
+        ]
+    };
+
+    updateAllComplete() {
+        this.allComplete =
+            this.participants.participant != null && this.participants.participant.every(t => t.completed);
+    }
+
+    someComplete(): boolean {
+        if (this.participants.participant == null) {
+            return false;
+        }
+        return this.participants.participant.filter(t => t.completed).length > 0 && !this.allComplete;
+    }
+
+    setAll(completed: boolean) {
+        this.allComplete = completed;
+        if (this.participants.participant == null) {
+            return;
+        }
+        this.participants.participant.forEach(t => (t.completed = completed));
+    }
 }
